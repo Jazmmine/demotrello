c6ec74a039153d0a7951eea82b0a71653a2709fd
Jazmmine <jazmine.angoma@gmail.com>
2016-09-22 11:47:57 -0500

    subiendo la primera version

---
+++ README.md
@@ -0,0 +1,7 @@
+#Demo Trello
+##Versión Uno
+1. En el HTML - elemento "añadir lista..."
+2. Cuando click elemento,
+	- input
+	- botón 
+	- esconder elemento 	
--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
-window.addEventListtener("load" function(){
+window.addEventListener("load", function(){
 	var nombreLista = document.getElementById("nombre-lista");
-	nombreLista.addEventListtener ("click" function(){
-		var mostrar
+	var eliminar = document.getElementById("eliminar");
+	var boton = document.getElementById("guardar");
+	nombreLista.addEventListener("click", function(){
+		document.getElementById("oculto").style.display = "block";
 	});
+
+	eliminar.addEventListener("click", function(){
+		document.getElementById("oculto").style.display = "none";
+	});
+
+
 });
